167f1f8ef09f3662e41b0db8eb00a43b9cc8ae91
ACM-Anik <anikmojumder5252@gmail.com>
2024-05-31 22:44:01 +0600

    EmailJS initial integration done

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 import contactPng from "../../assets/login.png";
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
+        publicKey: 'YOUR_PUBLIC_KEY',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
     <div className="mt-5">
       <div className="row d-flex align-items-center">
         <div className="col-md-6">
           <h1>Contact with Us</h1>
-          <div className="input-box">
+          {/* <div className="input-box">
             <input
               className="w-75 p-3 m-1"
               type="text"
@@ -29,8 +49,18 @@ const Contact = () => {
             <button class="btn btn-info w-75 p-3 font-weight-bold text-uppercase">
               Send Email
             </button>
-          </div>
+          </div> */}
+          <form ref={form} onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+          </form>
         </div>
+        {/* Side Poster------- */}
         <div className="col-md-6">
           <div className="w-100">
             <img
